68fa80d1a92f871faef08258cfb845692de0e086
Jonas Semmler <jonas.semmler@outlook.de>
2024-01-13 14:24:25 +0100

    improve styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Poppins } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import Image from 'next/image';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 export const metadata: Metadata = {
-  title: 'Step Recipe',
-  description: 'A recipe app for the modern web',
+  title:
+    'Chefkoch.de Rezepte als Schritt für Schritt Anleitung | kochschritt.de',
+  description:
+    'Eine einfache Schritt für Schritt Anleitung für jedes Chefkoch.de Rezept',
 };
 
 export default function RootLayout({
@@ -17,7 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        {children}
+        <div className="flex flex-col h-screen overflow-hidden">
+          <nav className="h-12 gap-2 flex-shrink-0 flex items-center px-6 bg-primary">
+            <Image
+              width={25}
+              height={25}
+              src="/favicon.svg"
+              alt="kochschritt.de"
+            />
+            <span className="text-lg font-bold text-white">kochschritt.de</span>
+          </nav>
+          <div className="overflow-auto grow">{children}</div>
+        </div>
         <Analytics />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as cheerio from 'cheerio';
 import Instruction from './components/Instruction';
 
@@ -17,6 +17,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [loadedRecipe, setLoadedRecipe] = useState(false);
   const [error, setError] = useState(String);
+  const myRef = useRef<null | HTMLDivElement>(null);
 
   const processSteps = (stepsString: string) => {
     const cheerioAPI = cheerio.load(stepsString);
@@ -59,101 +60,120 @@ export default function Home() {
     setLoading(false);
   };
 
+  useEffect(() => {
+    if (myRef.current) myRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, [recipeInstructions]);
+
   return (
-    <main className="flex min-h-screen container mx-auto flex-col items-center p-6 pt-14 sm:p-24 ">
+    <main className="flex min-h-full container mx-auto flex-col items-center p-6 pt-14 sm:p-24 ">
       <h1 className="text-5xl sm:text-6xl font-bold text-center">
         Schritt für Schritt
       </h1>
       <h2 className="text-xl sm:text-3xl mt-3 text-center text-gray-500">
         Jedes Chefkoch Rezept als konkrete Schritt-für-Schritt Anleitung
       </h2>
-      <div className="flex flex-col w-full md:flex-row items-center gap-2 mt-8 sm:mt-14">
-        <div className="flex flex-col w-full">
-          <label htmlFor="recipeLink" className="text-sm pb-1 text-gray-500">
-            Chefkoch Link
-          </label>
-          <input
-            id="recipeLink"
-            type="text"
-            value={recipeLink}
-            onChange={(e) => setRecipeLink(e.target.value)}
-            placeholder="Chefkoch Rezeptlink eingeben"
-            className="px-4 w-full sm:min-w-[400px] h-14 border border-gray-300 rounded-md"
-          />
-        </div>
-        <div className="flex flex-col w-full md:w-fit">
-          <label htmlFor="amount" className="text-sm pb-1 text-gray-500">
-            Portionen
-          </label>
-          <input
-            id="amount"
-            type="number"
-            value={amount}
-            max={100}
-            min={1}
-            onChange={(e) => {
-              let value = parseInt(e.target.value);
-              if (value > 100) setAmount(100);
-              else if (value < 1) setAmount(1);
-              else setAmount(value);
-            }}
-            className="px-4 h-14 border border-gray-300 rounded-md"
-          />
-        </div>
-        <button
-          disabled={loading}
-          onClick={handleGenerateInstructions}
-          className="self-end mt-4 sm:mt-0 w-full sm:w-fit leading-none h-14 px-4 py-2 bg-[#3B8047] hover:bg-green-900 text-white rounded-md"
+      <div className="flex flex-col grow w-full ">
+        <div
+          className={
+            'flex flex-col w-full md:flex-row transition-all items-center gap-2 mt-8 sm:mt-14' +
+            (!loading && !(recipeInstructions.length > 0) ? ' sm:mt-40' : '')
+          }
         >
-          Anleitung generieren
-        </button>
-      </div>
-      {loading && (
-        <div className="flex flex-col pt-14 items-center">
-          <svg
-            className="animate-spin w-14 h-14"
-            width="800px"
-            height="800px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <div className="flex flex-col w-full">
+            <label htmlFor="recipeLink" className="text-sm pb-1 text-gray-500">
+              Chefkoch Link
+            </label>
+            <input
+              id="recipeLink"
+              type="text"
+              value={recipeLink}
+              onChange={(e) => setRecipeLink(e.target.value)}
+              placeholder="Chefkoch Rezeptlink eingeben"
+              className="px-4 w-full sm:min-w-[400px] h-14 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div className="flex flex-col w-full md:w-fit">
+            <label htmlFor="amount" className="text-sm pb-1 text-gray-500">
+              Portionen
+            </label>
+            <input
+              id="amount"
+              type="number"
+              value={amount}
+              max={100}
+              min={1}
+              onChange={(e) => {
+                let value = parseInt(e.target.value);
+                if (value > 100) setAmount(100);
+                else if (value < 1) setAmount(1);
+                else setAmount(value);
+              }}
+              className="px-4 h-14 border border-gray-300 rounded-md"
+            />
+          </div>
+          <button
+            disabled={loading}
+            onClick={handleGenerateInstructions}
+            className="self-end mt-4 sm:mt-0 w-full sm:w-fit leading-none h-14 px-4 py-2 bg-primary hover:bg-green-900 text-white rounded-md"
           >
-            <path
-              opacity="0.2"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-              fill="#000000"
-            />
-            <path
-              d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
-              fill="#000000"
-            />
-          </svg>
-          <span className="text-gray-600 mt-4">
-            {loadedRecipe ? 'Erstelle Anleitung...' : 'Suche Rezept...'}
-          </span>
-          {loadedRecipe && (
-            <span className="text-gray-600 mt-2 text-sm">
-              Dies kann bis zu 1 Minute dauern
+            Anleitung generieren
+          </button>
+        </div>
+        {loading && (
+          <div className="flex flex-col pt-14 items-center">
+            <svg
+              className="animate-spin w-14 h-14"
+              width="800px"
+              height="800px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                opacity="0.2"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                fill="#000000"
+              />
+              <path
+                d="M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z"
+                fill="#000000"
+              />
+            </svg>
+            <span className="text-gray-600 mt-4">
+              {loadedRecipe ? 'Erstelle Anleitung...' : 'Suche Rezept...'}
             </span>
-          )}
-        </div>
-      )}
-      {error && (
-        <div className="bg-red-400 rounded-lg p-4 mt-10 text-red-900 w-fit">
-          {error}
-        </div>
-      )}
-      {recipeInstructions.length > 0 && (
-        <div className="mt-8 container">
-          <ul>
-            {recipeInstructions.map((instruction, index) => {
-              return <Instruction key={index} idx={index} text={instruction} />;
-            })}
-          </ul>
-        </div>
-      )}
+            {loadedRecipe && (
+              <span className="text-gray-600 mt-2 text-sm">
+                Dies kann bis zu 1 Minute dauern
+              </span>
+            )}
+          </div>
+        )}
+        {error && (
+          <div className="bg-red-400 rounded-lg p-4 mt-10 text-red-900 w-fit">
+            {error}
+          </div>
+        )}
+        {recipeInstructions.length > 0 && (
+          <div
+            ref={myRef}
+            className="pt-8 mt-8 border-gray-300 border-t  container"
+          >
+            <h2 className="text-4xl text-center font-bold mb-4">
+              Deine Anleitung
+            </h2>
+            <ul>
+              {recipeInstructions.map((instruction, index) => {
+                return (
+                  <Instruction key={index} idx={index} text={instruction} />
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
